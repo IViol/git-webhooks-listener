f2749ee082b7127409889369aad9759d90a6183c
Ivan Violentov <violentoff@gmail.com>
2017-09-18 13:38:57 +0300

    update server and package.json

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-import express from 'express'
-import { spawn } from 'child_process'
+const express = require('express')
+const bodyParser = require('body-parser')
+const { spawn } = require('child_process')
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
   console.log(`${req.method} ${req.originalUrl}`)
@@ -9,11 +12,35 @@ app.use(function(req, res, next) {
   next()
 })
 
-app.post('/push', function(req, res, next) {
+app.post('/*', function(req, res, next) {
+  const event = req.get('X-GitHub-Event')
+  const secret = req.get('X-Hub-Signature')// secret = sha1=956d8cac8effbcf8ca4fb9e1ef6f6b823a348774
+  const deliveryId = req.get('X-GitHub-Delivery')
+
+  const payload = req.body
+
+  console.log(`Event ${event} was fired`)
+  console.log(secret)
+  console.log(deliveryId)
+
+  if (event === 'ping') {
+    return ping(res)
+  } else if (event === 'push') {
+    return push(payload, res)
+  }
+
+  next()
+})
+
+function ping(res) {
+  res.status(200)
+}
+
+function push(payload, res) {
   const command = 'make'
   const args = ['push']
 
-  console.log('Request body is', req.body)
+  console.log('payload is', payload)
 
   console.log(`Child process ${command} ${args.join(' ')} is about to spawn`)
 
@@ -32,7 +59,7 @@ app.post('/push', function(req, res, next) {
 
     res.status(200).json({ success: true })
   })
-})
+}
 
 app.use(function(req, res, next) {
   res.status(404).json({ success: false, message: 'Listener for the hook not found' })
